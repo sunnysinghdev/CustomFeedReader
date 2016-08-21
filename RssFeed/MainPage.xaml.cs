a804b01e2b8d64432641f3556f76cd677c1227b5
sunnysinghdev <sunnysingh.vpcoe@gmail.com>
2016-08-21 17:24:46 +0530

    Addind source code

---
+++ RssFeed/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RssFeed
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            string s = @"zdfgdasfgfdg#TPSTART#
+#T#Appzillon#E#
+#H#Appzillon IDE#E#
+#D#Appzillon is a product#E#
+#S#Part of I-exceed company#E#
+#IURL#http://i-exceed.com/wp-content/uploads/2015/10/iexceed-logo_light.png#E#
+#NAV#http://i-exceed.com/#E#
+#NEXT#
+#T#Syd Bank#E#
+#H#App created for Syd Bank#E#
+#D#Powered by appzillon#E#
+#S#Work in Progress App#E#
+#IURL#http://i-exceed.com/wp-content/uploads/2016/03/syndicate-bank-logo1.png#E#
+#NAV#http://i-exceed.com/#E#
+#NEXT#
+#T#Quote#E#
+#H#Inspiration#E#
+#D#Inspirational Quotes#E#
+#S#Live Life Long#E#
+#NEXT#
+#TPEND#dfgsdfgs";
+            listView.ItemsSource = new TPLib().GetTPData(s);
+        }
+
+        private async void listView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            string a = e.ClickedItem.ToString();
+            await Windows.System.Launcher.LaunchUriAsync(new Uri((e.ClickedItem as TPObject).Nav));
+            Debug.WriteLine(a);
+        }
+    }
+}
---
+++ RssFeed/TPLib.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssFeed
+{
+    class TPLib
+    {
+        public List<TPObject> GetTPData(string str)
+        {
+            List<TPObject> li = new List<TPObject>();
+            str = str.Substring(str.IndexOf(TPSyntax.START)+ TPSyntax.START.Length, str.IndexOf(TPSyntax.CLOSE)- str.IndexOf(TPSyntax.START));
+            Debug.WriteLine(str);
+            string[] Articles = str.Split(new string[] { TPSyntax.SPLITTER }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string atricle in Articles) {
+                var ob = GetTPObject(atricle);
+                if (ob.isNull)
+                    continue;
+                li.Add(GetTPObject(atricle));
+            }
+
+            return li;
+
+        }
+        private TPObject GetTPObject(string article)
+        {
+
+            TPObject tp = new TPObject();
+            ArticleFilter ar = new ArticleFilter(article);
+
+            tp.Title = ar.GetAttributeText(TPSyntax.TITLE);
+            tp.Heading = ar.GetAttributeText(TPSyntax.HEADER);
+            tp.Description = ar.GetAttributeText(TPSyntax.DESCRIPTION);
+            tp.Story = ar.GetAttributeText(TPSyntax.STORY);
+            string url = ar.GetAttributeText(TPSyntax.IMG);
+            url = String.IsNullOrEmpty(url) ? "ms-appx:///Assets/StoreLogo.png" : url;
+            tp.ImgUrl = new Uri( url, UriKind.Absolute);
+            tp.Nav = ar.GetAttributeText(TPSyntax.NAV);
+            tp.isNull = String.IsNullOrEmpty(tp.Title) ? true : false;
+            Debug.WriteLine(tp.ToString());
+
+            return tp;
+        }
+       
+    }
+    class ArticleFilter {
+        string article = "";
+        
+        public ArticleFilter(string line) {
+            article = line;
+        }
+        public string GetAttributeText(string attr)
+        {
+            try
+            {
+                int index = article.IndexOf(attr) + attr.Length;
+                int length = article.IndexOf(TPSyntax.END) - article.IndexOf(attr) - attr.Length;
+                string line = article.Substring(index, length);
+                article = article.Substring(article.IndexOf(TPSyntax.END) + TPSyntax.END.Length);
+                return line;
+            }
+            catch (Exception) {
+                return null;
+            }
+        }
+
+    }
+    class TPObject {
+        public string Title { get; set; }
+        public string Heading { get; set; }
+        public string Description { get; set; }
+        public string Story { get; set; }
+        public Uri ImgUrl { get; set; }
+        public string Nav { get; set; }
+        public bool isNull { get; set; }
+        public override string ToString()
+        {
+            return "Title = "+Title+"\n"+
+                "Heading = " + Heading + "\n"+
+                "Description = "+Description + "\n"+
+                "Story = "+Story+"\n"+
+                "ImgUrl = "+ImgUrl + "\n"+
+                "Nav = " + Title + "\n";
+        }
+
+    }
+    class TPSyntax
+    {
+        public static string START = "#TPSTART#";
+        public static string CLOSE = "#TPEND#";
+        public static string TITLE = "#T#";
+        public static string HEADER = "#H#";
+        public static string DESCRIPTION = "#D#";
+        public static string STORY = "#S#";
+        public static string IMG = "#IURL#";
+        public static string NAV = "#NAV#";
+        public static string END = "#E#";
+        public static string SPLITTER = "#NEXT#";
+
+    }
+}
